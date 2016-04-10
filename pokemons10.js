6744f0ada450d3a5ee12ba5f76db4235977e34a6
Jasiunya <jasiunya@gmail.com>
2016-04-11 01:04:58 +0300

    Update pokemons10.js

--- pokemons10.js
+++ pokemons10.js
@@ -1,5 +1,5 @@
 var nextOffset = 0;
-var loadMoreButton;
+//var loadMoreButton;
 var offset = 0;
 
 //document.onLoad = function (){
@@ -20,7 +20,6 @@ var xmlhttp;
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       obj = JSON.parse(xmlhttp.responseText);
-
     }
       myCycle();
       nextOffset += 12;
@@ -41,13 +40,10 @@ function myCycle() {
 var out = "";
 var out2 = "";
     var i;
-    for(i = 0; i < obj.objects.length; i++) {
-
-
+    for(i = 0; i < 12; i++) {
         var pokemonID = obj.objects[i].national_id;
         out += ' <div class="col-xs-6 col-lg-4"><img src="http://pokeapi.co/media/img/' + obj.objects[i].national_id  + '.png"><h2>' + obj.objects[i].name + '</h2><p>Description. </p><p><button type="button" onclick="loadDetails(' + pokemonID + ')">View details</button></p></div><!--/.col-xs-6.col-lg-4-->';
         out2 += '<div class="attr" id="attr' + pokemonID + '" style="display:none"><img src="http://pokeapi.co/media/img/' + obj.objects[i].national_id  + '.png"><h2>' + obj.objects[i].name + " #" + obj.objects[i].national_id + '</h2><p>Type ' + obj.objects[i].national_id + '</p><p>Attack ' + obj.objects[i].attack + '</p><p>Defense ' + obj.objects[i].defense + '</p><p>HP ' + obj.objects[i].hp + '</p><p>SP Attack ' + obj.objects[i].sp_atk + '</p><p>SP Defense ' + obj.objects[i].sp_def + '</p><p>Speed ' + obj.objects[i].speed + '</p><p>Weight ' + obj.objects[i].weight + '</p><p>Total moves ' + obj.objects[i].total + '</p></div>';
-
 }
     document.getElementById("id02").innerHTML = out;
     //id02.insertAdjacentHTML('beforeend', out);
