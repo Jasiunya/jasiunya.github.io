ee03a7be4649df997f115d665052172f3a4d4e05
Jasiunya <jasiunya@gmail.com>
2016-04-12 01:48:48 +0300

    Update pokemons10.js

--- pokemons10.js
+++ pokemons10.js
@@ -48,21 +48,38 @@ $( document ).ready(function() {
                 var pokemonID = obj.objects[i].national_id;
                 out += ' <div class="col-xs-6 col-lg-4"><img src="http://pokeapi.co/media/img/' + obj.objects[i].national_id  + '.png"><h2>' + obj.objects[i].name + '</h2><p>Description. </p><p><button type="button" onclick="loadDetails(' + pokemonID + ')">View details</button></p></div><!--/.col-xs-6.col-lg-4-->';
                 out2 += '<div class="attr" id="attr' + pokemonID + '" style="display:none"><img src="http://pokeapi.co/media/img/' + obj.objects[i].national_id  + '.png"><h2>' + obj.objects[i].name + " #" + obj.objects[i].national_id + '</h2><p>Type ' + obj.objects[i].national_id + '</p><p>Attack ' + obj.objects[i].attack + '</p><p>Defense ' + obj.objects[i].defense + '</p><p>HP ' + obj.objects[i].hp + '</p><p>SP Attack ' + obj.objects[i].sp_atk + '</p><p>SP Defense ' + obj.objects[i].sp_def + '</p><p>Speed ' + obj.objects[i].speed + '</p><p>Weight ' + obj.objects[i].weight + '</p><p>Total moves ' + obj.objects[i].total + '</p></div>';
-                myTypes(i);
-            }
-            function myTypes(i) {
-                var out3 = "";
-                var i;
-                for(i = 0; i < obj.types.length; i++) {
-                out3 += '<p><a class="btn btn-default" href="" role="button">' + obj.types[i].name + '</a></p>';
-                document.getElementById("id05").innerHTML = out3;
+                var pokeUrl = 'http://pokeapi.co' + obj.objects[i].resource_uri;
+                loadXMLDoc2();
+                function loadXMLDoc2() {
+                    var xmlhttp;
+                    if (window.XMLHttpRequest) {
+                        xmlhttp = new XMLHttpRequest();
+                        } else {// code for older browsers
+                        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                        }
+                        xmlhttp.onreadystatechange = function() {
+                            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                                obj = JSON.parse(xmlhttp.responseText);
+                                myTypes(); 
+                                }
+                        }
+                        xmlhttp.open("GET", pokeUrl, true);
+                        xmlhttp.send();
                 }
-            }
+                function myTypes() {
+                    var out3 = "";
+                    var i;
+                    for(i = 0; i < obj.types.length; i++) {
+                    out3 += '<p><a class="btn btn-default" href="" role="button">' + obj.types[i].name + '</a></p>';
+                    document.getElementById("id05").innerHTML = out3;
+                    }
+                }
             // document.getElementById("id02").innerHTML = out;
             id02.insertAdjacentHTML('beforeend', out);
             
             //document.getElementById("id04").innerHTML = out2;
             id04.insertAdjacentHTML('beforeend', out2);
+            }
         }
     }
 });
