4a7618501f53f24a77babf03a30c982f941ab4a3
Jasiunya <jasiunya@gmail.com>
2016-04-12 02:03:49 +0300

    Update pokemons10.js

--- pokemons10.js
+++ pokemons10.js
@@ -70,7 +70,7 @@ $( document ).ready(function() {
                     }
                     function myTypes() {
                         var out3 = "";
-                        var i;
+                        //var i;
                         for(i = 0; i < obj2.types.length; i++) {
                         out3 += '<p><a class="btn btn-default" href="" role="button">' + obj2.types[i].name + '</a></p>';
                         document.getElementById("id05").innerHTML = out3;
