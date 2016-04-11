d3cd1c8e6b041fa93b92d5bfe7a8b830c98e8865
Jasiunya <jasiunya@gmail.com>
2016-04-12 00:42:12 +0300

    Update pokemons10.js

--- pokemons10.js
+++ pokemons10.js
@@ -48,7 +48,15 @@ $( document ).ready(function() {
                 out += ' <div class="col-xs-6 col-lg-4"><img src="http://pokeapi.co/media/img/' + obj.objects[i].national_id  + '.png"><h2>' + obj.objects[i].name + '</h2><p>Description. </p><p><button type="button" onclick="loadDetails(' + pokemonID + ')">View details</button></p></div><!--/.col-xs-6.col-lg-4-->';
                 out2 += '<div class="attr" id="attr' + pokemonID + '" style="display:none"><img src="http://pokeapi.co/media/img/' + obj.objects[i].national_id  + '.png"><h2>' + obj.objects[i].name + " #" + obj.objects[i].national_id + '</h2><p>Type ' + obj.objects[i].national_id + '</p><p>Attack ' + obj.objects[i].attack + '</p><p>Defense ' + obj.objects[i].defense + '</p><p>HP ' + obj.objects[i].hp + '</p><p>SP Attack ' + obj.objects[i].sp_atk + '</p><p>SP Defense ' + obj.objects[i].sp_def + '</p><p>Speed ' + obj.objects[i].speed + '</p><p>Weight ' + obj.objects[i].weight + '</p><p>Total moves ' + obj.objects[i].total + '</p></div>';
             }
-
+            function mytypes() {
+                var out3 = "";
+                var i;
+                for(i = 0; i < obj.types.length; i++) {
+                    out3 = obj.types[i].name + " ";
+                }
+    document.getElementById("id04").innerHTML = out2;
+}
+}
             // document.getElementById("id02").innerHTML = out;
             id02.insertAdjacentHTML('beforeend', out);
             
