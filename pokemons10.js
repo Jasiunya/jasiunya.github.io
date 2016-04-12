c10f92b5e596a634e55798fc45cb492aaf3a8d49
Jasiunya <jasiunya@gmail.com>
2016-04-12 22:43:10 +0300

    Update pokemons10.js

--- pokemons10.js
+++ pokemons10.js
@@ -45,40 +45,22 @@ $( document ).ready(function() {
         if(obj != undefined)
         {
             for(var i = 0; i < obj.objects.length; i++) {
-                var pokeUrl = 'http://pokeapi.co' + obj.objects[i].resource_uri;
-                pokeTypes();
-                function pokeTypes() {
-                    loadXMLDoc2();
-                    function loadXMLDoc2() {
-                        var xmlhttp;
-                        if (window.XMLHttpRequest) {
-                            xmlhttp = new XMLHttpRequest();
-                            } else {// code for older browsers
-                            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-                            }
-                            xmlhttp.onreadystatechange = function() {
-                                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                                    obj2 = JSON.parse(xmlhttp.responseText);
-                                    myTypes(); 
-                                    }
-                            }
-                            xmlhttp.open("GET", pokeUrl, true);
-                            xmlhttp.send();
-                    }
-                    function myTypes() {
-                        if(obj2 != undefined) {
-                            var out3 = "";
-                            var u;
-                            for(u = 0; u < obj2.types.length; u++) {
-                            out3 += '<p><a class="btn btn-default" href="" role="button">' + obj2.types[u].name + '</a></p>';
+                var obj2 = obj.types;
+                myTypes(); 
+                function myTypes() {
+                    if(obj2 != undefined) {
+                        var out3 = "";
+                        var i;
+                        for(i = 0; i < obj2.types.length; i++) {
+                            out3 += '<p><a class="btn btn-default" href="" role="button">' + obj2.types[i].name + '</a></p>';
                             //document.getElementById("id05").innerHTML = out3;
-                            }
                         }
                     }
                 }
-                var pokemonID = obj.objects[i].national_id;
-                out += ' <div class="col-xs-6 col-lg-4"><img src="http://pokeapi.co/media/img/' + obj.objects[i].national_id  + '.png"><h2>' + obj.objects[i].name + '</h2><p>' + out3 + '</p><p><button type="button" onclick="loadDetails(' + pokemonID + ')">View details</button></p></div><!--/.col-xs-6.col-lg-4-->';
-                out2 += '<div class="attr" id="attr' + pokemonID + '" style="display:none"><img src="http://pokeapi.co/media/img/' + obj.objects[i].national_id  + '.png"><h2>' + obj.objects[i].name + " #" + obj.objects[i].national_id + '</h2><p>Type ' + obj.objects[i].national_id + '</p><p>Attack ' + obj.objects[i].attack + '</p><p>Defense ' + obj.objects[i].defense + '</p><p>HP ' + obj.objects[i].hp + '</p><p>SP Attack ' + obj.objects[i].sp_atk + '</p><p>SP Defense ' + obj.objects[i].sp_def + '</p><p>Speed ' + obj.objects[i].speed + '</p><p>Weight ' + obj.objects[i].weight + '</p><p>Total moves ' + obj.objects[i].total + '</p></div>';
+            }
+            var pokemonID = obj.objects[i].national_id;
+            out += ' <div class="col-xs-6 col-lg-4"><img src="http://pokeapi.co/media/img/' + obj.objects[i].national_id  + '.png"><h2>' + obj.objects[i].name + '</h2><p>' + out3 + '</p><p><button type="button" onclick="loadDetails(' + pokemonID + ')">View details</button></p></div><!--/.col-xs-6.col-lg-4-->';
+            out2 += '<div class="attr" id="attr' + pokemonID + '" style="display:none"><img src="http://pokeapi.co/media/img/' + obj.objects[i].national_id  + '.png"><h2>' + obj.objects[i].name + " #" + obj.objects[i].national_id + '</h2><p>Type ' + obj.objects[i].national_id + '</p><p>Attack ' + obj.objects[i].attack + '</p><p>Defense ' + obj.objects[i].defense + '</p><p>HP ' + obj.objects[i].hp + '</p><p>SP Attack ' + obj.objects[i].sp_atk + '</p><p>SP Defense ' + obj.objects[i].sp_def + '</p><p>Speed ' + obj.objects[i].speed + '</p><p>Weight ' + obj.objects[i].weight + '</p><p>Total moves ' + obj.objects[i].total + '</p></div>';
 
                 
             // document.getElementById("id02").innerHTML = out;
@@ -86,7 +68,6 @@ $( document ).ready(function() {
             
             //document.getElementById("id04").innerHTML = out2;
             id04.insertAdjacentHTML('beforeend', out2);
-            }
         }
     }
 });
